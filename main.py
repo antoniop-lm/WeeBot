6c5a26ccdf1ee4f112ad3bb854e56edf79d50905
antoniop-lm <antoniop.lm@hotmail.com>
2024-02-08 20:10:50 -0300

    Menu Handler

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
 from typing import Final
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, KeyboardButton
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 import json
 import logging
 
+# Config
 f = open('credentials.json')
 creds = json.load(f)
 TOKEN: Final = creds['TELEGRAM_TOKEN']
 BOT_USERNAME: Final = creds['BOT_USERNAME']
 
+# Menu
+menu = [
+    "Anime",
+    "List",
+    "Update",
+    "Follow",
+    "Ping"
+]
+
+subMenu = {
+    "Anime": ["Track","Untrack"],
+    "Follow": ["Subscribe","Unsubscribe"]
+}
+
 # Commands
 async def start_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-    buttons = [[KeyboardButton("Help")], [KeyboardButton("Track")]]
-    await update.message.reply_text('Hello! Bondo likes you! Hope we have fun!')
+    buttons = []
+    for option in menu:
+        buttons.append([KeyboardButton(option)])
+    await context.bot.send_message(chat_id=update.effective_chat.id, 
+                                   text='Hello! Bondo likes you! Hope we have fun!',
+                                   reply_markup=ReplyKeyboardMarkup(buttons))
+
+async def options_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    buttons = []
+    for option in menu:
+        buttons.append([KeyboardButton(option)])
+    await context.bot.send_message(chat_id=update.effective_chat.id, 
+                                   text='Let me help you',
+                                   reply_markup=ReplyKeyboardMarkup(buttons))
     
 async def help_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('What can I sniff you in?')
@@ -21,10 +48,15 @@ async def custom_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Throw the ball, please!')
 
 # Responses
-    
 def handle_response(text: str) -> str:
     processed: str = text.lower()
 
+    if text in menu:
+        return text
+    if text in subMenu["Anime"]:
+        return text
+    if text in subMenu["Follow"]:
+        return text
     if 'hello' in processed:
         return 'Haaai!'
     if 'how are you' in processed:
@@ -48,11 +80,23 @@ async def handle_message (update: Update, context: ContextTypes.DEFAULT_TYPE):
         response: str = handle_response(text)
 
     logging.info('Bot: %s', response)
-    await update.message.reply_text(response)
+    
+    buttons = []
+    if subMenu.get(response):
+        for subOption in subMenu.get(response):
+            buttons.append([KeyboardButton(subOption)])
+    
+    if buttons:
+        await context.bot.send_message(chat_id=update.effective_chat.id, 
+                                   text='Let me help you',
+                                   reply_markup=ReplyKeyboardMarkup(buttons))
+    else:
+        await update.message.reply_text(response)
 
 async def error (update: Update, context: ContextTypes.DEFAULT_TYPE):
     logging.info(f'Update ({update}) caused error {context.error}')
 
+# Main
 if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     logging.basicConfig(
@@ -63,6 +107,7 @@ if __name__ == '__main__':
 
     # Commands
     app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('options', options_command))
     app.add_handler(CommandHandler('help', help_command))
     app.add_handler(CommandHandler('custom', custom_command))
 
